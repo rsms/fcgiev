d2195c9a158a9ee7d8910f769ccca87b305e0d46
Rasmus Andersson <rasmus@flajm.com>
2009-04-28 02:12:26 +0200

    works!

--- test1.c
+++ test1.c
@@ -14,8 +14,9 @@
 #define FLAG_KEEP_CONN 1
 #define REQUESTS_MAX __SHRT_MAX__+__SHRT_MAX__
 
-static void (*bufferevent_readcb)(int, short, void *) = NULL;
-static void (*bufferevent_writecb)(int, short, void *) = NULL;
+#define ROLE_RESPONDER  1
+#define ROLE_AUTHORIZER 2
+#define ROLE_FILTER     3
 
 
 typedef struct {
@@ -24,14 +25,20 @@ typedef struct {
 
 
 typedef struct {
-  uint16_t ident;
+  uint16_t id;
+  uint16_t role;
+  uint8_t keepconn;
+  uint8_t stdin_eof;
+  uint8_t terminate;
+  uint8_t padding;
+  struct bufferevent *bev;
 } request_t;
 
 
 request_t *_requests[REQUESTS_MAX];
 
 
-typedef enum {
+enum {
   TYPE_BEGIN_REQUEST     =  1,
   TYPE_ABORT_REQUEST     =  2,
   TYPE_END_REQUEST       =  3,
@@ -43,15 +50,15 @@ typedef enum {
   TYPE_GET_VALUES        =  9,
   TYPE_GET_VALUES_RESULT = 10,
   TYPE_UNKNOWN           = 11
-} message_type_t;
+};
 
 
-typedef enum {
-  REQUEST_COMPLETE = 0,
-  CANT_MPX_CONN    = 1,
-  OVERLOADED       = 2,
-  UNKNOWN_ROLE     = 3
-} protocol_status_t;
+enum {
+  PROTOST_REQUEST_COMPLETE = 0,
+  PROTOST_CANT_MPX_CONN    = 1,
+  PROTOST_OVERLOADED       = 2,
+  PROTOST_UNKNOWN_ROLE     = 3
+};
 
 
 typedef struct {
@@ -63,7 +70,7 @@ typedef struct {
   uint8_t contentLengthB0;
   uint8_t paddingLength;
   uint8_t reserved;
-} header_t;
+} header_t; // 8
 
 
 typedef struct {
@@ -71,7 +78,7 @@ typedef struct {
   uint8_t roleB0;
   uint8_t flags;
   uint8_t reserved[5];
-} begin_request_t;
+} begin_request_t; // 8
 
 
 typedef struct {
@@ -82,18 +89,19 @@ typedef struct {
   uint8_t appStatusB0;
   uint8_t protocolStatus;
   uint8_t reserved[3];
-} end_request_t;
+} end_request_t; // 16
 
 
 typedef struct {
   header_t header;
   uint8_t type;
   uint8_t reserved[7];
-} unknown_type_t;
+} unknown_type_t; // 16
 
 
-void header_init(header_t *self, message_type_t t, uint16_t id, uint16_t len) {
-  self->version = t;
+inline static void header_init(header_t *self, uint8_t t, uint16_t id, uint16_t len) {
+  self->version = '\1';
+  self->type = t;
   self->requestIdB1 = id >> 8;
   self->requestIdB0 = id & 0xff;
   self->contentLengthB1 = len >> 8;
@@ -103,49 +111,51 @@ void header_init(header_t *self, message_type_t t, uint16_t id, uint16_t len) {
 }
 
 
-void end_request_init(end_request_t *self, uint16_t id, uint32_t ast, protocol_status_t pst) {
+inline static void end_request_init(end_request_t *self, uint16_t id, uint32_t ast, uint8_t protostatus) {
   header_init((header_t *)self, TYPE_END_REQUEST, id, sizeof(end_request_t)-sizeof(header_t));
   self->appStatusB3 = (ast >> 24) & 0xff;
   self->appStatusB3 = (ast >> 16) & 0xff;
   self->appStatusB3 = (ast >> 8) & 0xff;
   self->appStatusB3 = ast & 0xff;
-  self->protocolStatus = pst;
+  self->protocolStatus = protostatus;
   memset(self->reserved, 0, sizeof(self->reserved));
 }
 
 
-void unknown_type_init(unknown_type_t *self, uint8_t unknown_type) {
+inline static void unknown_type_init(unknown_type_t *self, uint8_t unknown_type) {
   header_init((header_t *)self, TYPE_UNKNOWN, 0, sizeof(unknown_type_t)-sizeof(header_t));
   self->type = unknown_type;
   memset(self->reserved, 0, sizeof(self->reserved));
 }
 
 
-#define BEV_FD(p) ((p)->bev.ev_read.ev_fd)
-#define BEV_FD_SET(p, v) ((p)->bev.ev_read.ev_fd = (v))
+/////////// bev
 
 
-// get a request by request id
-request_t *request_get(uint16_t id) {
-  request_t *r;
-  r = _requests[id];
-  if (r == NULL) {
-    r = (request_t *)calloc(1, sizeof(request_t));
-    _requests[id] = r;
-  }
-  return r;
-}
+#define BEV_FD(p) ((p)->ev_read.ev_fd)
+#define BEV_FD_SET(p, v) ((p)->ev_read.ev_fd = (v))
 
-// restore a request object
-void request_put(request_t *r) {
-  // eeeh... nothing.
+
+inline static void bev_disable(struct bufferevent *bev) {
+  int events = 0;
+  if (bev->readcb)
+    events |= EV_READ;
+  if (bev->writecb)
+    events |= EV_WRITE;
+  bufferevent_disable(bev, events);
 }
 
 
-void bev_close(struct bufferevent *bev) {
-  bufferevent_disable(bev);
+inline static void bev_close(struct bufferevent *bev) {
+  bev_disable(bev);
 	close(BEV_FD(bev));
   BEV_FD_SET(bev, -1);
+}
+
+
+inline static void bev_drain(struct bufferevent *bev) {
+  evbuffer_drain(bev->input, EVBUFFER_LENGTH(bev->input));
+  evbuffer_drain(bev->output, EVBUFFER_LENGTH(bev->output));
 }
 
 
@@ -155,10 +165,84 @@ void bev_abort(struct bufferevent *bev) {
   lingeropt.l_linger = 0;
   AZ(setsockopt(BEV_FD(bev), SOL_SOCKET, SO_LINGER, (char *)&lingeropt, sizeof(lingeropt)));
   shutdown(BEV_FD(bev), SHUT_RDWR);
-  evbuffer_drain(bev->input, EVBUFFER_LENGTH(bev->input));
-  evbuffer_drain(bev->output, EVBUFFER_LENGTH(bev->output));
+  bev_drain(bev);
   bev_close(bev);
 }
+
+
+inline static int bev_add(struct event *ev, int timeout) {
+	struct timeval tv, *ptv = NULL;
+	if (timeout) {
+		evutil_timerclear(&tv);
+		tv.tv_sec = timeout;
+		ptv = &tv;
+	}
+	return event_add(ev, ptv);
+}
+
+
+/////////// request
+
+
+// get a request by request id
+inline static request_t *request_get(uint16_t id) {
+  request_t *r;
+  r = _requests[id];
+  if (r == NULL) {
+    r = (request_t *)calloc(1, sizeof(request_t));
+    _requests[id] = r;
+  }
+  printf("** get req %p\n", r);
+  return r;
+}
+
+// restore a request object
+inline static void request_put(request_t *r) {
+  printf("** put req %p\n", r);
+  r->bev->cbarg = NULL;
+  r->bev = NULL;
+}
+
+
+void request_write(request_t *r, const char *buf, uint16_t len, uint8_t tostdout) {
+  if(len == 0)
+    return;
+  
+  header_t h;
+  header_init(&h, tostdout ? TYPE_STDOUT : TYPE_STDERR, r->id, len);
+  
+  if (evbuffer_add(r->bev->output, (const void *)&h, sizeof(header_t)) != -1)
+    evbuffer_add(r->bev->output, (const void *)buf, len);
+  
+  // schedule write
+  if (r->bev->enabled & EV_WRITE)
+		bev_add(&r->bev->ev_write, r->bev->timeout_write);
+}
+
+
+void request_end(request_t *r, uint32_t appstatus, uint8_t protostatus) {
+  uint8_t buf[32]; // header + header + end_request_t
+  uint8_t *p = buf;
+  
+  //assert(EVBUFFER_LENGTH(r->bev->output) == 0);
+  
+  // Terminate the stdout and stderr stream, and send the end-request message.
+  header_init((header_t *)p, TYPE_STDOUT, r->id, 0);
+  p += sizeof(header_t);
+  header_init((header_t *)p, TYPE_STDERR, r->id, 0);
+  p += sizeof(header_t);
+  end_request_init((end_request_t *)p, r->id, appstatus, protostatus);
+  p += sizeof(end_request_t);
+  
+  bufferevent_write(r->bev, (const void *)buf, sizeof(buf));
+  
+  r->terminate = true;
+}
+
+
+void app_handle_beginrequest(request_t *r);
+void app_handle_input(request_t *r, uint16_t length);
+void app_handle_requestaborted(request_t *r);
 
 
 void fcgiproto_errorcb(struct bufferevent *bev, short what, request_t *r) {
@@ -175,6 +259,126 @@ void fcgiproto_errorcb(struct bufferevent *bev, short what, request_t *r) {
 }
 
 
+inline static void process_unknown(struct bufferevent *bev, uint8_t type, uint16_t len) {
+  printf("process_unknown(%p, %d, %d)\n", bev, type, len);
+  unknown_type_t msg;
+  unknown_type_init(&msg, type);
+  bufferevent_write(bev, (const void *)&msg, sizeof(unknown_type_t));
+  evbuffer_drain(bev->input, sizeof(header_t) + len);
+}
+
+
+inline static void process_begin_request(struct bufferevent *bev, uint16_t id, const begin_request_t *br) {
+  request_t *r;
+  
+  r = request_get(id);
+  assert(r->bev == NULL);
+  
+  r->bev = bev;
+  r->id = id;
+  r->keepconn = (br->flags & FLAG_KEEP_CONN) == 1;
+  r->role = (br->roleB1 << 8) + br->roleB0;
+  r->stdin_eof = false;
+  r->terminate = false;
+  bev->cbarg = (void *)r;
+  
+  evbuffer_drain(bev->input, sizeof(header_t)+sizeof(begin_request_t));
+}
+
+
+inline static void process_abort_request(struct bufferevent *bev, uint16_t id) {
+  request_t *r;
+  
+  r = request_get(id);
+  assert(r->bev != NULL);
+  
+  bev_drain(bev);
+  bev_disable(bev);
+  
+  app_handle_requestaborted(r);
+  
+  request_put(r);
+}
+
+
+inline static void process_params(struct bufferevent *bev, uint16_t id, const uint8_t *buf, uint16_t len) {
+  request_t *r;
+  
+  r = request_get(id);
+  assert(r->bev != NULL);
+  
+  // Is this the last message to come? Then queue the request for the user.
+  if (len == 0) {
+    evbuffer_drain(bev->input, sizeof(header_t));
+    app_handle_beginrequest(r);
+    return;
+  }
+  
+  // Process message.
+
+  uint8_t const * const bufend = buf + len;
+  uint32_t name_len;
+  uint32_t data_len;
+  
+  while(buf != bufend) {
+    
+    if (*buf >> 7 == 0) {
+      name_len = *(buf++);
+    }
+    else {
+      name_len = ((buf[0] & 0x7F) << 24) + (buf[1] << 16) + (buf[2] << 8) + buf[3];
+      buf += 4;
+    }
+    
+    if (*buf >> 7 == 0) {
+      data_len = *(buf++);
+    }
+    else {
+      data_len = ((buf[0] & 0x7F) << 24) + (buf[1] << 16) + (buf[2] << 8) + buf[3];
+      buf += 4;
+    }
+    
+    assert(buf + name_len + data_len <= bufend);
+    
+    // todo replace with actual adding to req:
+    char k[255], v[8192];
+    strncpy(k, (const char *)buf, name_len); k[name_len] = '\0';
+    buf += name_len;
+    strncpy(v, (const char *)buf, data_len); v[data_len] = '\0';
+    buf += data_len;
+    printf("fcgiproto>> param>> '%s' => '%s'\n", k, v);
+    // todo: req->second->params[name] = data;
+  }
+  
+  evbuffer_drain(bev->input, sizeof(header_t) + len);
+}
+
+
+inline static void process_stdin(struct bufferevent *bev, uint16_t id, const uint8_t *buf, uint16_t len) {
+  request_t *r;
+  
+  r = request_get(id);
+  
+  // left-over stdin on inactive request is drained and forgotten
+  if (r->bev == NULL) {
+    bev_drain(bev);
+    return;
+  }
+  
+  assert(r->bev != NULL);
+  evbuffer_drain(bev->input, sizeof(header_t));
+  
+  // Is this the last message to come? Then set the eof flag.
+  // Otherwise, add the data to the buffer in the request structure.  
+  if (len == 0) {
+    r->stdin_eof = true;
+    return;
+  }
+  
+  app_handle_input(r, len);
+}
+
+
 void fcgiproto_readcb(struct bufferevent *bev, request_t *r) {
   printf("fcgiproto_readcb(%p, %p)\n", bev, r);
   //bufferevent_write_buffer(bev, bev->input);
@@ -185,8 +389,7 @@ void fcgiproto_readcb(struct bufferevent *bev, request_t *r) {
     // Check whether our peer speaks the correct protocol version.
     if (hp->version != 1) {
       warnx("fcgiev: cannot handle protocol version %u", hp->version);
-      request_abort(r);
-      evbuffer_drain(bev->input, EVBUFFER_LENGTH(bev->input));
+      bev_abort(bev);
       break;
     }
     
@@ -200,13 +403,51 @@ void fcgiproto_readcb(struct bufferevent *bev, request_t *r) {
       return;
     
     // Process the message.
-    // todo
+    printf("fcgiproto>> received message: id: %d, bodylen: %d, type: %d\n",
+      msg_id, msg_len, (int)hp->type);
+    
+    switch (hp->type) {
+      case TYPE_BEGIN_REQUEST:
+        process_begin_request(bev, msg_id, 
+          (const begin_request_t *)(EVBUFFER_DATA(bev->input) + sizeof(header_t)) );
+        break;
+      case TYPE_ABORT_REQUEST:
+        process_abort_request(bev, msg_id);
+        break;
+      case TYPE_PARAMS:
+        process_params(bev, msg_id, (const uint8_t *)EVBUFFER_DATA(bev->input) + sizeof(header_t), msg_len);
+        break;
+      case TYPE_STDIN:
+        process_stdin(bev, msg_id, (const uint8_t *)EVBUFFER_DATA(bev->input) + sizeof(header_t), msg_len);
+        break;
+      //case TYPE_END_REQUEST:
+      //case TYPE_STDOUT:
+      //case TYPE_STDERR:
+      //case TYPE_DATA:
+      //case TYPE_GET_VALUES:
+      //case TYPE_GET_VALUES_RESULT:
+      //case TYPE_UNKNOWN:
+      default:
+        process_unknown(bev, hp->type, msg_len);
+    }/* switch(hp->type) */
+    
   }
 }
 
 
 void fcgiproto_writecb(struct bufferevent *bev, request_t *r) {
+  // Invoked if bev->output is drained or below the low watermark.
   printf("fcgiproto_writecb(%p, %p)\n", bev, r);
+  
+  if (r != NULL && r->terminate) {
+    bev_disable(r->bev);
+    bev_drain(r->bev);
+    if (r->keepconn == false) {
+      printf("fcgiproto_writecb: closing connection\n");
+      bev_close(r->bev);
+    }
+    request_put(r);
+  }
 }
 
 
@@ -276,14 +517,35 @@ void server_run(server_t *server) {
 
 
 void fcgiev_init() {
-  struct bufferevent *bev;
-  if (bufferevent_readcb != NULL)
-    return;
-  bev = bufferevent_new(0, (evbuffercb)NULL, (evbuffercb)NULL, (everrorcb)NULL, NULL);
-  bufferevent_readcb = bev->ev_read.ev_callback;
-  bufferevent_writecb = bev->ev_write.ev_callback;
-  bufferevent_free(bev);
   memset((void *)_requests, 0, sizeof(_requests));
+}
+
+
+void app_handle_beginrequest(request_t *r) {
+  printf("app_handle_beginrequest %p\n", r);
+  
+  if (r->role != ROLE_RESPONDER) {
+    request_write(r, "We can't handle any role but RESPONDER.", 39, 0);
+    request_end(r, 1, PROTOST_UNKNOWN_ROLE);
+    return;
+  }
+  
+  static const char hello[] = "Content-type: text/plain\r\n\r\nHello world\n";
+  request_write(r, hello, sizeof(hello)-1, 1);
+  
+  request_end(r, 0, PROTOST_REQUEST_COMPLETE);
+}
+
+
+void app_handle_input(request_t *r, uint16_t length) {
+  printf("app_handle_input %p -- %d bytes\n", r, length);
+  // simply drain it for now
+  evbuffer_drain(r->bev->input, length);
+}
+
+
+void app_handle_requestaborted(request_t *r) {
+  printf("app_handle_requestaborted %p\n", r);
 }
 
 
